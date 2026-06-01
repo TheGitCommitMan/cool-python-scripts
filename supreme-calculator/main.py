e51742d2c7735b902038a9bf21332404269e5993
TheGitCommitMan <theflashfan74@gmail.com>
2026-06-01 13:20:47 +0100

    Add supreme-calculator module

---
+++ supreme-calculator/main.py
@@ -0,0 +1,9 @@
+# Requesting Input From User and defining their inputs to specific variables
+num1=int(input("Please enter your first number: ")) # num1 is defined by user input
+num2=int(input("Great Choice! Please enter your second number: ")) # num2 is defined by user input
+
+# Calculating The Sum
+num3=num1+num2
+
+# Displaying the sum
+print("Your answer is:",num3) #this one
