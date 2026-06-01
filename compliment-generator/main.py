6acb0da4896358a80aa375ca8bffccb852f5a05c
TheGitCommitMan <theflashfan74@gmail.com>
2026-06-01 13:20:55 +0100

    Add compliment-generator module

---
+++ compliment-generator/main.py
@@ -0,0 +1,47 @@
+# Simply said, it gives compliments. neednt more be said
+
+user=input('What is your name? ')
+if user=='debug':
+    print("how do you know about this?")
+if user=='leo':
+    print("the developer is also called leo lol")
+if user=='Leo':
+    print("The developer is also called that")
+else:
+    print("nice to meet you,",user)
+number=input("enter a number between 1-10 for a lovely compliment: ")
+if number=='1':
+    print("Everyone loves",user)
+    
+if number=='2':
+    print("you look amazing,",user)
+    
+if number=='3':
+    print("You have a great sense of humor,",user)
+    
+if number=='4':
+    print("You are perfect as you are,",user)
+    
+if number=='5':
+    print("Never let anybody tell you that your not good enough,",user)
+    
+if number=='6':
+    print("You can do anything you put your mind to,",user)
+    
+if number=='7':
+    print("You are loved,",user)
+    
+if number=='8':
+    print("Oh My God, It is you! The amazing",user)
+    
+if number=='9':
+    print("you are amazing",user)
+    
+if number=='10':
+    print("I hope your day is as perfect as you,",user)
+
+if number=='debug':
+    print("You shouldn't know this code, but yes, the code runs. as you can see.")
+
+else: 
+    print("EXECUTION COMPLETED     Please restart the program to continue: I am too lazy to prevent this from printing on valid runs, if your number was 1-10 good. otherwise try again if you want a compliment")
