1bd85de92fc7501cbe801b4868d119a095c97dd6
TheGitCommitMan <theflashfan74@gmail.com>
2026-06-01 13:20:15 +0100

    Add phrase-repeater module

---
+++ phrase-repeater/main.py
@@ -0,0 +1,25 @@
+# Defining our variables
+n=0 # This tells python that n is defined as 0
+userInput=int(input("How many times do you want to print it? "))
+userPhrase=input("Enter a phrase: ") # tells python to ask the user to input a phrase
+
+# Repeats the phrase the requested amount of times
+while n <userInput: # a while loop
+    print(userPhrase)
+    n+= 1 # increments it
+    
+print("execution complete XD")
+    
+    
+    
+    
+    
+    
+    
+    
+                    ## Developer's Note ## 
+# This code took a long time to debug haha but i ended up getting some help from copilot 
+# i used study and learn mode so that it asked questions 
+# which helped me understand it and not just copy and paste it 
+# i would still consider this to be human coded because i only used ai for help
+# glad it works now
